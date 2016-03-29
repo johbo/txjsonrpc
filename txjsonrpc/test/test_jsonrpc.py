04569bff1fe66865694ecfd10b8d7638dd31d089
Paul Hummer <paul@eventuallyanyway.com>
2016-03-29 12:43:42 -0600

    Add more test coverage.

--- txjsonrpc/test/test_jsonrpc.py
+++ txjsonrpc/test/test_jsonrpc.py
@@ -1,37 +1,80 @@
 import json
 
+from twisted.internet import defer
 from twisted.trial.unittest import TestCase
 
+from txjsonrpc import jsonrpc
 from txjsonrpc.jsonrpc import BaseProxy, BaseQueryFactory
 from txjsonrpc.jsonrpclib import Fault, VERSION_PRE1, VERSION_1, VERSION_2
+
+
+class BaseSubhandlerTestCase(TestCase):
+    """Tests for txjsonrpc.jsonrpc.BaseSubhandler."""
+
+    def setUp(self):
+        self.subhandler = jsonrpc.BaseSubhandler()
+
+    def test_init(self):
+        """An empty dict of subhandlers is added."""
+        self.assertEqual({}, self.subhandler.subHandlers)
+
+    def test_putSubHandler(self):
+        """A subhandler is added."""
+        self.subhandler.putSubHandler('test', 'foo-bar-baz')
+
+        self.assertEqual(
+            'foo-bar-baz', self.subhandler.subHandlers['test'])
+
+    def test_getSubHandler(self):
+        """A subhandler is retrieved."""
+        self.subhandler.subHandlers['test'] = 'foo-bar-baz'
+
+        result = self.subhandler.getSubHandler('test')
+
+        self.assertEqual('foo-bar-baz', result)
+
+    def test_getSubHandlerPrefixes(self):
+        """A list of subhandlers prefixes is returned."""
+        self.subhandler.subHandlers['test-a'] = 'foo-bar-baz'
+        self.subhandler.subHandlers['test-b'] = 'foo-bar-baz'
+        self.subhandler.subHandlers['test-c'] = 'foo-bar-baz'
+
+        result = self.subhandler.getSubHandlerPrefixes()
+
+        self.assertEqual(
+            ['test-a', 'test-b', 'test-c'], sorted(result))
 
 
 class BaseQueryFactoryTestCase(TestCase):
 
     def test_creation(self):
+        """Properties are set on the factory on creation."""
         factory = BaseQueryFactory("someMethod")
-        self.assertTrue(factory.payload is not None)
-        self.assertTrue(factory.deferred is not None)
+        payload = json.loads(factory.payload)
+
+        self.assertEqual(VERSION_PRE1, factory.version)
+        self.assertEqual(1, factory.id)
+        self.assertTrue(isinstance(factory.deferred, defer.Deferred))
+        self.assertEqual(
+            {'method': 'someMethod', 'params': []}, payload)
 
     def test_buildVersionedPayloadPre1(self):
-        expected = {'params': [], 'method': ''}
+        expected = {'params': [], 'method': 'someMethod'}
 
         factory = BaseQueryFactory("someMethod", version=VERSION_PRE1)
-        payload = factory._buildVersionedPayload()
-        json_payload = json.loads(payload)
+        json_payload = json.loads(factory.payload)
 
         self.assertEquals(expected, json_payload)
 
     def test_buildVersionedPayload1(self):
         expected = {
             'params': [],
-            'method': '',
+            'method': 'someMethod',
             'id': 1
             }
 
         factory = BaseQueryFactory("someMethod", version=VERSION_1)
-        payload = factory._buildVersionedPayload()
-        json_payload = json.loads(payload)
+        json_payload = json.loads(factory.payload)
 
         self.assertEquals(expected, json_payload)
 
@@ -39,37 +82,30 @@ class BaseQueryFactoryTestCase(TestCase):
         expected = {
             'params': [],
             'jsonrpc': '2.0',
-            'method': '',
+            'method': 'someMethod',
             'id': 1
             }
 
         factory = BaseQueryFactory("someMethod", version=VERSION_2)
-        payload = factory._buildVersionedPayload()
-        json_payload = json.loads(payload)
+        json_payload = json.loads(factory.payload)
 
         self.assertEquals(expected, json_payload)
 
     def test_parseResponseNoJSON(self):
-
-        def check_error(error):
-            self.assertEquals(
-                str(error.value), "No JSON object could be decoded")
-
         factory = BaseQueryFactory("someMethod")
         d = factory.deferred
         factory.parseResponse("oops")
-        return d.addErrback(check_error)
+        self.assertFailure(d, ValueError)
+        return d
 
     def test_parseResponseRandomJSON(self):
-
-        def check_result(result):
-            self.assertEquals(
-                result, {u'something': 1})
+        expected = {'something': 1}
 
         factory = BaseQueryFactory("someMethod")
         d = factory.deferred
-        factory.parseResponse('{"something": 1}')
-        return d.addCallback(check_result)
+        factory.parseResponse(json.dumps({'something': 1}))
+        return d.addCallback(
+            lambda result: self.assertEquals(expected, result))
 
     def test_parseResponseFaultData(self):
 
@@ -80,8 +116,8 @@ class BaseQueryFactoryTestCase(TestCase):
 
         factory = BaseQueryFactory("someMethod")
         d = factory.deferred
-        factory.parseResponse(
-            '{"fault": "Fault", "faultCode": 1, "faultString": "oops"}')
+        factory.parseResponse(json.dumps(
+            {"fault": "Fault", "faultCode": 1, "faultString": "oops"}))
         return d.addErrback(check_error)
 
 
@@ -120,3 +156,61 @@ class BaseProxyTestCase(TestCase):
         proxy = BaseProxy()
         factoryClass = proxy._getFactoryClass({"factoryClass": FakeFactory})
         self.assertEquals(factoryClass, FakeFactory)
+
+
+class FakeHandler(jsonrpc.BaseSubhandler):
+    """A Fake Handler for Introspection tests."""
+
+    def jsonrpc_test(self, paramA, paramB):
+        """A test method that does nothing."""
+    jsonrpc_test.signature = [None, str, int]
+
+
+class IntrospectionTestCase(TestCase):
+    """Tests for txjsonrpc.jsonrpc.Introspection."""
+
+    def test_init(self):
+        """Initial properties are set on creation."""
+        parent = FakeHandler()
+        api = jsonrpc.Introspection(parent)
+
+        self.assertEqual({}, api.subHandlers)  # From BaseSubHandler
+        self.assertEqual(parent, api._jsonrpc_parent)
+
+    def test_jsonrpc_listMethods(self):
+        """A list of supported method names is returned."""
+        api = jsonrpc.Introspection(FakeHandler())
+
+        methods = api.jsonrpc_listMethods()
+
+        self.assertEqual(['test'], methods)
+
+    def test_jsonrpc_methodHelp(self):
+        """A docstring is returned."""
+        api = jsonrpc.Introspection(FakeHandler())
+
+        doc = api.jsonrpc_methodHelp('test')
+
+        self.assertEqual(
+            'A test method that does nothing.', doc)
+
+    def test_jsonrpc_methodSignature(self):
+        """The function signature is returned."""
+        api = jsonrpc.Introspection(FakeHandler())
+
+        doc = api.jsonrpc_methodSignature('test')
+
+        self.assertEqual(
+            [None, str, int], doc)
+
+
+class AddIntrospectionTestCase(TestCase):
+    """Tests for txjsonrpc.jsonrpc.addIntrospection."""
+
+    def test_addIntrospection(self):
+        """A 'system' handler is installed."""
+        handler = FakeHandler()
+
+        jsonrpc.addIntrospection(handler)
+
+        self.assertIn('system', handler.subHandlers)
--- txjsonrpc/test/test_jsonrpclib.py
+++ txjsonrpc/test/test_jsonrpclib.py
@@ -1,9 +1,30 @@
+from datetime import datetime
 import json
 
 from twisted.trial.unittest import TestCase
 from twisted.internet import defer
+from txjsonrpc import jsonrpclib
 from txjsonrpc.jsonrpclib import (
     Fault, VERSION_PRE1, VERSION_1, VERSION_2, dumps, loads)
+
+
+class JSONRPCEncoderTestCase(TestCase):
+    """Tests for txjsonrpc.jsonrpclib.JSONRPCEncoder."""
+
+    def test_default(self):
+        """A date is returned in string format."""
+        date = datetime.now()
+
+        encoder = jsonrpclib.JSONRPCEncoder()
+        result = encoder.default(date)
+
+        self.assertEquals(date.strftime("%Y%m%dT%H:%M:%S"), result)
+
+    def test_default_not_a_date(self):
+        """A non-date param raises TypeError."""
+        encoder = jsonrpclib.JSONRPCEncoder()
+
+        self.assertRaises(TypeError, encoder.default, 'string')
 
 
 class DumpTestCase(TestCase):
@@ -52,6 +73,20 @@ class DumpTestCase(TestCase):
 
         data = {"some": "data"}
         result = dumps(data, version=VERSION_1)
+        json_result = json.loads(result)
+
+        self.assertEquals(expected, json_result)
+
+    def test_unknown_version(self):
+        """An unknown version acts the same as JSONRPC 1.0."""
+        expected = {
+            "id": None,
+            "result": {"some": "data"},
+            "error": None
+            }
+
+        data = {"some": "data"}
+        result = dumps(data, version="JSON-RPC 95")
         json_result = json.loads(result)
 
         self.assertEquals(expected, json_result)
@@ -121,3 +156,89 @@ class LoadsTestCase(TestCase):
 
             dl.append(d)
         return defer.DeferredList(dl, fireOnOneErrback=True)
+
+
+class SimpleParserTestCase(TestCase):
+    """Tests for txjsonrpc.jsonrpclib.SimpleParser."""
+
+    def test_feed(self):
+        """Data is appended to the parser buffer."""
+        parser = jsonrpclib.SimpleParser()
+        parser.buffer = 'abc'
+
+        parser.feed('def')
+
+        self.assertEqual('abcdef', parser.buffer)
+
+    def test_close(self):
+        expected = {'test': 'close'}
+        parser = jsonrpclib.SimpleParser()
+        parser.buffer = json.dumps(expected)
+
+        parser.close()
+
+        self.assertEquals(expected, parser.data)
+
+
+class SimpleUnmarshallerTestCase(TestCase):
+    """Tests for txjsonrpc.jsonrpclib.SimpleUnmarshaller."""
+
+    def setUp(self):
+        self.unmarshaller = jsonrpclib.SimpleUnmarshaller()
+        self.unmarshaller.parser = jsonrpclib.SimpleParser()
+        self.unmarshaller.parser.data = {
+            'id': 1000,
+            'method': 'getMethodName',
+            'params': ['a', 'b', 'c'],
+            }
+
+    def test_getmethodname(self):
+        """The name of the method is returned."""
+        name = self.unmarshaller.getmethodname()
+
+        self.assertEqual('getMethodName', name)
+
+    def test_getid(self):
+        """The id of the data is returned."""
+        id = self.unmarshaller.getid()
+
+        self.assertEqual(1000, id)
+
+    def test_close(self):
+        """Params are returned when parser data is a dict."""
+        data = self.unmarshaller.close()
+
+        self.assertEqual(['a', 'b', 'c'], data)
+
+    def test_close_raw_data(self):
+        """Raw data is returned when parser data is not a dict."""
+        self.unmarshaller.parser.data = 'Raw test data'
+
+        data = self.unmarshaller.close()
+
+        self.assertEqual('Raw test data', data)
+
+
+class GetParserTestCase(TestCase):
+    """Tests for txjsonrpc.jsonrpclib.getparser"""
+
+    def test_getparser(self):
+        """A parser and marshaller are returned."""
+        parser, unmarshaller = jsonrpclib.getparser()
+
+        self.assertTrue(isinstance(parser, jsonrpclib.SimpleParser))
+        self.assertTrue(isinstance(unmarshaller, jsonrpclib.SimpleUnmarshaller))
+        self.assertIs(parser, unmarshaller.parser)
+
+
+class TransportTestCase(TestCase):
+    """Tests for txjsonrpc.jsonrpclib.Transport."""
+
+    def test_getparser(self):
+        """A parser and marshaller are returned."""
+        transport = jsonrpclib.Transport()
+        parser, unmarshaller = transport.getparser()
+
+        self.assertTrue(isinstance(parser, jsonrpclib.SimpleParser))
+        self.assertTrue(isinstance(unmarshaller, jsonrpclib.SimpleUnmarshaller))
+        self.assertIs(parser, unmarshaller.parser)
